ae87d1a596cb387471a74a60ace1f66244d8777f
Ky Phan <thekypccc@gmail.com>
2025-09-03 23:53:25 +0700

    feat: template for Food

--- main.cpp
+++ main.cpp
@@ -37,3 +37,38 @@ public:
     }
 };
 
+class Food {
+public:
+    Vector2 position;
+    Texture2D texture;
+
+    Food(deque <Vector2> snakeBody) {
+        Image image = LoadImage("Graphics/food.png");
+        texture = LoadTextureFromImage(image);
+        UnloadImage(image);
+        position = GenerateRandomPos(snakeBody);
+    }
+
+    // destruct
+    ~Food() {
+        UnloadTexture(texture);
+    }
+
+    void Draw() {
+        DrawTexture(texture, offset + position.x * cellSize, offset + position.y * cellSize, WHITE);
+    }
+
+    Vector2 GenerateRandomCell() {
+        float x = GetRandomValue(0, cellCount - 1);
+        float y = GetRandomValue(0, cellCount - 1);
+        return Vector2{x, y};
+    }
+
+    Vector2 GenerateRandomPos(deque <Vector2> snakeBody) {
+        Vector2 position = GenerateRandomCell();
+        while (ElementInDeque(position, snakeBody)) {
+            position = GenerateRandomCell();
+        }
+        return position;
+    }
+};
