8d068575b697e942aade4061a733bb123b4d0de9
Ky Phan <thekypccc@gmail.com>
2025-09-06 23:22:45 +0700

    feat: add helpers

--- main.cpp
+++ main.cpp
@@ -12,7 +12,23 @@ int cellCount = 28;
 int offset = 75;
 double lastUpdateTime = 0;
 
+bool ElementInDeque(Vector2 element, deque<Vector2> deque) {
+    for (unsigned int i = 0; i < deque.size(); i++) {
+        if (Vector2Equals(deque[i], element)) {
+            return true;
+        }
+    }
+    return false;
+}
 
+bool eventTriggered(double interval) {
+    double currentTime = GetTime();
+    if (currentTime - lastUpdateTime >= interval) {
+        lastUpdateTime = currentTime;
+        return true;
+    }
+    return false;
+}
 
 class Snake {
 public:
